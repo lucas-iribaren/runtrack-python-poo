9cf48672045886a3ceede5c4c4cde8ad76a8b726
lucas-iribaren <lucas.iribaren@laplateforme.io>
2024-01-02 14:29:06 +0100

    job06 méthode veillir

--- jour01/job06.py
+++ jour01/job06.py
@@ -1,19 +1,19 @@
 class Animal:
-    def __init__(self, prenom):
+    def __init__(self):
         self.age = 0
-        self.prenom = prenom
 
     def vieillir(self):
         self.age += 1
 
-mon_animal = Animal(prenom="Nom_de_l'animal")
+
+mon_animal = Animal()
 
 # Afficher l'âge initial
 print("Âge initial de l'animal :", mon_animal.age)
 
 # Faire vieillir l'animal
 mon_animal.vieillir()
-print("# Age de l'animal apres appel de la méthode vieillir")
+print("# Age de l'animal après appel de la méthode vieillir")
 
 # Afficher le nouvel âge
 print("Nouvel âge de l'animal :", mon_animal.age)
